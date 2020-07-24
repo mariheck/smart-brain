9553f5e9e23f90071564c90475d0243acc82055c
Marine Heckler <marine.heckler@gmail.com>
2020-07-24 21:00:31 +0200

    Updates

---
+++ precache-manifest.be3db86a96a0b991eb23def17723eead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836ce49176046221eff831d583052724",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "bb7cb6862d1734d31296",
+    "url": "/smart-brain/static/css/main.c402a77b.chunk.css"
+  },
+  {
+    "revision": "42723a485da1f31ce0de",
+    "url": "/smart-brain/static/js/2.9e43ef6b.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smart-brain/static/js/2.9e43ef6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb7cb6862d1734d31296",
+    "url": "/smart-brain/static/js/main.678920ba.chunk.js"
+  },
+  {
+    "revision": "f713852481949665bf23",
+    "url": "/smart-brain/static/js/runtime-main.850e91ee.js"
+  }
+]);
